12f8d5eb1c011625d6c5ef3978889e08333249f0
tjwngml <tjwngml0306@gmail.com>
2025-12-09 13:41:41 +0900

    TodoList-UI구현

---
+++ section08/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Editor from './Components/Editor'
+import Header from './Components/Header'
+import List from './Components/List'
+
+function App() {
+
+  return (
+    <div className='App'>
+    <Header/>
+    <Editor/>
+    <List/>
+    </div>
+  );
+}
+
+export default App;
