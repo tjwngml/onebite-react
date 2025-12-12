7205200299cba1372a1f4f91523dc5430a37bc46
tjwngml <tjwngml0306@gmail.com>
2025-12-12 10:15:03 +0900

    TodoList - Read( 투두리스트 렌더링하기)

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,16 +1,54 @@
 
+import { useState, useRef } from 'react'
 import './App.css'
 import Editor from './Components/Editor'
 import Header from './Components/Header'
 import List from './Components/List'
 
+  const mockData =[
+    {
+    id:0,
+    isDone : false,
+    content :"React 공부하기",
+    date : new Date().getTime(),
+  },
+   {
+    id:1,
+    isDone : false,
+    content : "빨래하기",
+    date : new Date().getTime(),
+  },
+   {
+    id:2,
+    isDone : false,
+    content :"노래 연습하기",
+    date : new Date().getTime(),
+  },
+];
+
 function App() {
+
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3)
+
+  const onCreate = (content)=>{
+    const newTodo = {
+      id : idRef.current++,
+      inDone : false,
+      content: content,
+      date : new Date().getTime()
+    }
+
+    setTodos([newTodo,...todos]);
+
+  }
+
 
   return (
     <div className='App'>
     <Header/>
-    <Editor/>
-    <List/>
+    <Editor onCreate={onCreate}/>
+    <List todos={todos}/>
     </div>
   );
 }
--- section08/src/Components/List.jsx
+++ section08/src/Components/List.jsx
@@ -1,15 +1,33 @@
+import { use, useState } from 'react';
 import './List.css';
 import TodoItem from './TodoItem';
 
-function List(){
+function List({todos}){
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if(search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+  );
+  };
+
+   const filteredTodos = getFilteredData();
+
   return(
     <div className="List">
       <h4>Todo List ❤️</h4>
-      <input placeholder="검색어를 입력하세요"/>
+      <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요"/>
       <div className='todos_wrapper'>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
+        {filteredTodos.map((todo)=>{
+          return <TodoItem key={todo.id} {...todo}/>;
+        })}
       </div>
     </div>
   );
