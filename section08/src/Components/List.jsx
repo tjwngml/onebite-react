7cdc0df1228ca16b0f04ab8eb2ef76a3d8921075
tjwngml <tjwngml0306@gmail.com>
2025-12-12 10:26:53 +0900

    TodoList  - Update ( 투두 수정하기 )

--- section08/src/Components/List.jsx
+++ section08/src/Components/List.jsx
@@ -2,7 +2,7 @@ import { use, useState } from 'react';
 import './List.css';
 import TodoItem from './TodoItem';
 
-function List({todos}){
+function List({todos, onUpdate}){
   const [search, setSearch] = useState("");
 
   const onChangeSearch = (e) => {
@@ -26,7 +26,7 @@ function List({todos}){
       <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요"/>
       <div className='todos_wrapper'>
         {filteredTodos.map((todo)=>{
-          return <TodoItem key={todo.id} {...todo}/>;
+          return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate}/>;
         })}
       </div>
     </div>
--- section08/src/Components/TodoItem.jsx
+++ section08/src/Components/TodoItem.jsx
@@ -1,9 +1,14 @@
 import './TodoItem.css';
 
-function TodoItem({id,isDone,content,date}){
+function TodoItem({id,isDone,content,date, onUpdate}){
+  
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  }
+
   return(
     <div className="TodoItem">
-      <input readOnly checked={isDone} type="checkbox"/>
+      <input onChange={onChangeCheckbox} readOnly checked={isDone} type="checkbox"/>
       <div className='content'>{content}</div>
       <div className='date'>{new Date(date).toLocaleDateString()}</div>
       <button>삭제</button>
