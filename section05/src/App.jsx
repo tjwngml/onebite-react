384c0ed946f5ba3f7888240e8e70dc6b6c0611a5
tjwngml <tjwngml0306@gmail.com>
2025-12-13 19:01:44 +0900

    5.6) State로 상태관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,11 +1,12 @@
+import { useState } from 'react';
 import './App.css'
-import HookExam from './components/HookExam';
-import Register from './components/Register';
 
 function App() {
+  const [state , setState] = useState(0);
   return (
     <> 
-      <HookExam/>
+    <h1>{state}</h1>
+    <button onClick={()=>{setState(state+1)}}>+</button>
     </>
   );
 }
