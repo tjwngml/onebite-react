ec6202df18bfd7705ecfa43823e23db4d7140b37
tjwngml <tjwngml0306@gmail.com>
2025-12-17 14:30:53 +0900

    페이지 라우팅 : 페이지 이동

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,5 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Route, Routes ,Link, useNavigate } from 'react-router-dom'
 import Diary from './pages/Diary'
 import Home from './pages/Home'
 import New from './pages/New'
@@ -11,8 +11,20 @@ import Notfound from './pages/Notfound'
 
 function App() {
 
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  }
+
   return (
-    
+    <>
+    <div>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/new"}>New</Link>
+      <Link to={"/diary"}>Diary</Link>
+    </div>
+    <button onClick={onClickButton}>New 페이지로 이동</button>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/new' element={<New/>}/>
@@ -22,6 +34,7 @@ function App() {
 
         
       </Routes>
+    </>
   
   )
 }
