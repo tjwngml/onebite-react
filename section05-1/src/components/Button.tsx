ea53e0c93ec8c7c3b93285c34b69d63c89b4c08a
tjwngml <tjwngml0306@gmail.com>
2025-12-18 14:46:40 +0900

    일기 관리 기능 구현하기 1

---
+++ section05-1/src/components/Button.tsx
@@ -0,0 +1,18 @@
+function Button({children, text , color = "black"}){
+// 이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  }
+
+  return(
+    <button
+    onClick={onClickButton}
+    // onMouseEnter={onClickButton}
+    style={{color:color}}>{text} - {color}
+    {children}
+    </button>
+  );
+}
+
+export default Button;
--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,53 +1,43 @@
 import './App.css'
-import { Route, Routes ,Link, useNavigate } from 'react-router-dom'
+import { Route, Routes  } from 'react-router-dom'
 import Diary from './pages/Diary'
 import Home from './pages/Home'
 import New from './pages/New'
 import Notfound from './pages/Notfound'
-import { getEmotionImage } from './util/get-emotion-images'
-import Button from './Components/Button'
-import Header from './Components/Header'
+import Edit from './pages/Edit'
+import { useReducer } from 'react'
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2/ "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "diary" : 일기를 상세히 조회하는 Diary 페이지
+
+const mockData = [
+  {
+    id: 1,
+    createdDate : new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate : new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  }
+]
+
+function reducer(state,action){
+  return state;
+}
 
 function App() {
-
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  }
+  const [data, dispatch] = useReducer(reducer,[mockData]);
 
   return (
     <>
-    <Header title={"Header"}
-      leftChild={<Button text={"Left"} />}
-      rightChild={<Button text={"Right"}/>}
-      />
-
-
-
-
-
-
-    <Button text={123}
-    onClick={()=>{console.log("123번 버튼 클릭!");}}/>
-
-
-    <Button text={123}
-    type={"POSITIVE"} 
-    onClick={()=>{console.log("123번 버튼 클릭!");}}/>
-
-     <Button text={123}
-    type={"NEGATIVE"} 
-    onClick={()=>{console.log("123번 버튼 클릭!");}}/>
-    
+  
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/new' element={<New/>}/>
         <Route path='/diary/:id' element={<Diary/>}/>
+        <Route path='/edit/:id' element={<Edit/>}/>
         <Route path='*' element={<Notfound/>}/>
 
 
