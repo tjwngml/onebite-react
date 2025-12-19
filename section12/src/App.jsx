d0a51a6d08f7ba16381506fbae6f07e1568371db
tjwngml <tjwngml0306@gmail.com>
2025-12-19 13:33:11 +0900

    일기 관리 기능 구현하기 2

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home'
 import New from './pages/New'
 import Notfound from './pages/Notfound'
 import Edit from './pages/Edit'
-import { useReducer } from 'react'
+import { createContext, useReducer, useRef } from 'react'
 
 
 const mockData = [
@@ -24,15 +24,70 @@ const mockData = [
 ]
 
 function reducer(state,action){
-  return state;
+  switch(action.type){
+    case 'CREATE' : return [action.data,...state];
+    case 'UPDATE' : return state.map((item)=>item.id === action.data.id? action.data :item);
+    case 'DELETE' : return state.filter((item)=> String(item.id) !== String(action.id));
+    default :
+    return state;
+  }
 }
 
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
+
 function App() {
-  const [data, dispatch] = useReducer(reducer,[mockData]);
+  const [data, dispatch] = useReducer(reducer,mockData);
+  const idRef = useRef(3);
+
+  // 새로운 일기 추가
+  const onCreate = (createdDate,emotionId,content) => {
+    dispatch({
+      type: "CREATE",
+      data : {
+        id : idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      }
+    });
+  }
+  
+  // 기존 일기 수정
+  const onUpdate = (id,createdDate,emotionId,content) => {
+    dispatch({
+      type: "UPDATE",
+      data : {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      }
+    })
+  }
+
+
+
+  // 기존 일기 삭제
+  const onDelete = (id) => {    
+    dispatch({
+      type : "DELETE",
+      id,
+    })
+
+  }
+
 
   return (
     <>
-  
+    <button onClick={()=>{onCreate(new Date().getTime(),1,"Hello")}}>일기 추가 테스트</button>
+    <button onClick={()=>{onUpdate(1,new Date().getTime,3,"수정된 일기입니다");}}>일기 수정 테스트</button>
+    <button onClick={()=>{onDelete(1);}}>일기 삭제 테스트</button>
+
+    <DiaryStateContext.Provider value={data}>
+    <DiaryDispatchContext.Provider value={{onCreate,onUpdate,onDelete}}>
+
+
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/new' element={<New/>}/>
@@ -43,6 +98,8 @@ function App() {
 
         
       </Routes>
+    </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
     </>
   
   )
